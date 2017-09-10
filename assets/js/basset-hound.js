aa098a2e6c83fe4d169b7221d38a46c87124042e
Joy <joymccaffreychicago@gmail.com>
2017-09-10 12:32:56 -0500

    Add outer nose and move outer head to left to realign

--- assets/js/basset-hound.js
+++ assets/js/basset-hound.js
@@ -5,7 +5,7 @@ var ctx = canvas.getContext("2d");
 // keep for reference
 
 ctx.beginPath();
-ctx.arc(290, 150, 125, 0, 2 * Math.PI);
+ctx.arc(280, 150, 130, 0, 2 * Math.PI);
 ctx.fillStyle="#c77321";
 ctx.fill();
 ctx.strokeStyle="#c77321";
@@ -22,15 +22,42 @@ ctx.fill();
 ctx.lineWidth = 1;
 ctx.strokeStyle="#b4c2cd";
 ctx.stroke();
-ctx.closePath();
 
 // left eye
 // moveTo start position
 // bezierCurveTo x, y of left angle, x, y of right angle, end position
 
 ctx.beginPath();
-ctx.moveTo(216, 145);
-ctx.bezierCurveTo(219, 154, 245, 153, 241, 133);
+ctx.moveTo(216, 144);
+ctx.bezierCurveTo(219, 154, 245, 153, 241, 132);
 ctx.fillStyle="black";
 ctx.fill();
+ctx.stroke();
+
+// outer right eye
+
+ctx.beginPath();
+ctx.moveTo(346, 150);
+ctx.lineTo(295, 125);
+ctx.lineTo(337, 195);
+ctx.fillStyle="#b4c2cd";
+ctx.fill();
+ctx.lineWidth = 1;
+ctx.strokeStyle="#b4c2cd";
+ctx.stroke();
+
+// outer nose
+ctx.beginPath();
+ctx.moveTo(275, 93);
+ctx.lineTo(261, 159);
+ctx.lineTo(217, 206);
+ctx.lineTo(209, 315);
+ctx.lineTo(275, 297);
+ctx.lineTo(341, 315);
+ctx.lineTo(332, 206);
+ctx.lineTo(288, 159);
+ctx.lineTo(275, 93);
+ctx.lineWidth = 1;
+// ctx.strokeStyle="#f8f1cc";
+ctx.strokeStyle="black";
 ctx.stroke();
