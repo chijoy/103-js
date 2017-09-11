6ae99ed9a0f76c0591d2bcd03bc154c4e8eacd2c
Ali Karbassi <ali@karbassi.com>
2017-09-11 09:59:36 -0500

    Adding coding conventions.
    - Moved colors out to a const variable `COLOR`
    - All strings are in single quotes
    - `fill()` and `fillStyle` are after `closePath()`
    - `stroke()` and `strokeStyle` are after `closePath()`
    - Removed redundant `stroke`s when fill is the same color

--- assets/js/basset-hound.js
+++ assets/js/basset-hound.js
@@ -1,81 +1,61 @@
-var canvas = document.getElementById("basset-hound");
-var ctx = canvas.getContext("2d");
+var canvas = document.getElementById('basset-hound');
+var ctx = canvas.getContext('2d');
 
-// outer circle of face
-// keep for reference
+const COLOR = {
+  BODY: '#AC7A34',
+  EYE: '#BAC1CC',
+  PUPIL: '#221F20',
+  NOSE: '#221F20',
+  MUSSEL: '#F5F0CE',
+  EAR: '#62391A'
+};
 
-// ctx.beginPath();
-// ctx.arc(275, 158, 130, 0, 2 * Math.PI);
-// ctx.fillStyle="#c77321";
-// ctx.fill();
-// ctx.strokeStyle="black";
-// ctx.stroke();
-
-// head
-
+// HEAD
 ctx.beginPath();
 ctx.moveTo(177, 75);
 ctx.bezierCurveTo(227, 15, 324, 15, 374, 75);
 ctx.lineTo(351, 195);
 ctx.lineTo(341, 315);
 ctx.lineTo(210, 315);
-ctx.fillStyle="#c77321";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.BODY;
+ctx.fill();
 
-// outer left eye
-
+// OUTER LEFT EYE
 ctx.beginPath();
 ctx.moveTo(204, 150);
 ctx.lineTo(256, 125);
 ctx.lineTo(213, 195);
-ctx.fillStyle="#b4c2cd";
-ctx.fill();
-ctx.lineWidth = 1;
-ctx.strokeStyle="#b4c2cd";
-ctx.stroke();
 ctx.closePath();
+ctx.fillStyle = COLOR.EYE;
+ctx.fill();
 
-// left eye
-// moveTo start position
-// bezierCurveTo x, y of left angle, x, y of right angle, end position
-
+// LEFT EYE
 ctx.beginPath();
 ctx.moveTo(216, 144);
 ctx.bezierCurveTo(219, 154, 245, 153, 241, 132);
-ctx.fillStyle="black";
-ctx.fill();
-ctx.stroke();
 ctx.closePath();
+ctx.fillStyle = COLOR.PUPIL;
+ctx.fill();
 
-// outer right eye
-
+// OUTER RIGHT EYE
 ctx.beginPath();
 ctx.moveTo(346, 150);
 ctx.lineTo(295, 125);
 ctx.lineTo(337, 195);
-ctx.fillStyle="#b4c2cd";
-ctx.fill();
-ctx.lineWidth = 1;
-ctx.strokeStyle="#b4c2cd";
-ctx.stroke();
 ctx.closePath();
+ctx.fillStyle = COLOR.EYE;
+ctx.fill();
 
-// right eye
-// moveTo start position
-// bezierCurveTo x, y of left angle, x, y of right angle, end position
-
+// RIGHT EYE
 ctx.beginPath();
 ctx.moveTo(309, 132);
 ctx.bezierCurveTo(306, 153, 332, 154, 333, 144);
-ctx.fillStyle="black";
-ctx.fill();
-ctx.strokeStyle="black";
-ctx.stroke();
 ctx.closePath();
+ctx.fillStyle = COLOR.PUPIL;
+ctx.fill();
 
-// outer nose
-
+// OUTER NOSE
 ctx.beginPath();
 ctx.moveTo(275, 93);
 ctx.lineTo(261, 159);
@@ -86,12 +66,11 @@ ctx.lineTo(341, 315);
 ctx.lineTo(332, 206);
 ctx.lineTo(288, 159);
 ctx.lineTo(275, 93);
-ctx.fillStyle="#f8f1cc";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.MUSSEL;
+ctx.fill();
 
-// nose
-
+// NOSE
 ctx.beginPath();
 ctx.moveTo(275, 200);
 ctx.lineTo(242, 213);
@@ -100,12 +79,11 @@ ctx.lineTo(275, 261);
 ctx.lineTo(308, 253);
 ctx.lineTo(308, 213);
 ctx.lineTo(275, 200);
-ctx.fillStyle="black";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.NOSE;
+ctx.fill();
 
-// left ear
-
+// LEFT EAR
 ctx.beginPath();
 ctx.moveTo(177, 75);
 ctx.lineTo(200, 195);
@@ -115,22 +93,20 @@ ctx.lineTo(236, 464);
 ctx.lineTo(173, 464);
 ctx.bezierCurveTo(140, 463, 110, 450, 103, 315);
 ctx.lineTo(177, 75);
-ctx.fillStyle="#76300e";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.EAR;
+ctx.fill();
 
-// body
-
+// BODY
 ctx.beginPath();
 ctx.moveTo(448, 315);
 ctx.lineTo(548, 464);
 ctx.lineTo(378, 464);
-ctx.fillStyle="#c77321";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.BODY;
+ctx.fill();
 
-// right ear
-
+// RIGHT EAR
 ctx.beginPath();
 ctx.moveTo(374, 75);
 ctx.lineTo(351, 195);
@@ -140,19 +116,17 @@ ctx.lineTo(315, 464);
 ctx.lineTo(378, 464);
 ctx.bezierCurveTo(411, 463, 441, 450, 448, 315);
 ctx.lineTo(374, 75);
-ctx.fillStyle="#76300e";
-ctx.fill();
 ctx.closePath();
+ctx.fillStyle = COLOR.EAR;
+ctx.fill();
 
-// neck
-
+// NECK
 ctx.beginPath();
 ctx.moveTo(236, 464);
 ctx.lineTo(315, 464);
 ctx.lineTo(315, 307);
 ctx.lineTo(275, 297);
 ctx.lineTo(236, 307);
-ctx.fillStyle="#c77321";
-ctx.fill();
 ctx.closePath();
-
+ctx.fillStyle = COLOR.BODY;
+ctx.fill();
