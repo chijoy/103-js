34ecc1a32dc32234fd6070687d5d109b44ce1968
Joy <joymccaffreychicago@gmail.com>
2017-09-16 18:44:01 -0500

    Initial commit

---
+++ assets/js/tapir.js
@@ -0,0 +1,13 @@
+var canvas = document.getElementById('tapir');
+var ctx = canvas.getContext('2d');
+
+const COLOR = {
+    BODY: '#AC7A34',
+    EYE: '#BAC1CC',
+    PUPIL: '#221F20',
+    NOSE: '#221F20',
+    MUSSEL: '#F5F0CE',
+    EAR: '#62391A'
+  };
+  
+  // HEAD
